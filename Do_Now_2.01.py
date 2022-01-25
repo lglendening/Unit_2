40b81f17e36c7e7dcbf992ca1f49f1846ecd5f72
Logan G <lpglendening25@students.bmchs.org>
2022-01-25 09:43:44 -0600

    done with do now and lab

---
+++ Do_Now_2.01.py
@@ -0,0 +1,22 @@
+'''
+############################
+Do Now 2.01
+############################
+
+Open up the console and type the following:
+
+x_stage1 = 123
+y_stage1 = 456
+
+x_stage2 = str(x_stage1)
+y_stage2 = str(y_stage1)
+
+print(x_stage2 + y_stage2)
+
+---------------------------------
+Answer the following questions:
+1. What type are the variables x_stage1 and y_stage1? they are variiabes
+2. What type are the variables x_stage2 and y_stage2? thay tell the program that its a string
+3. How would you convert a string, '100' to an integer? string = str(100)
+
+'''

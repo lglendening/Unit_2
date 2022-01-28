e023556f164ef10b6d7a98b35db60695dd57f310
Logan G <lpglendening25@students.bmchs.org>
2022-01-28 09:45:22 -0600

    working with lab

---
+++ Lab 2.03.py
@@ -0,0 +1,74 @@
+'''
+##########################################
+Lab 2.03 - Game Show
+##########################################
+In your Notebook
+Follow the flow of execution in the following programs and predict what will happen for each
+one
+---------------------------------------
+Example 1
+---------------------------------------
+a = input("What... is your quest")
+b = "to seek the holy grail"
+if a != b:
+    print("Go On. Off you go")
+else:
+    b = input("What...is the air-speed velocity of an unladen swallow?")
+    if b == "What do you mean? An African or European swallow?":
+        print("I don't know that...AHHH [Bridgekeeper was thrown over bridge]")
+    else:
+        print("[you were thrown over bridge]")
+
+Prediction:     [you were thrown over bridge]
+Actual:         error for me
+
+---------------------------------------
+Example 2
+---------------------------------------
+user_input = input("What is your favorite color")
+if user_input == 'blue':
+    print("Blueskadoo")
+elif user_input == "red":
+    print("Roses are red!")
+elif user_input == "yellow":
+    print("Mellow Yellow")
+elif user_input == "green":
+    print("Green Machine")
+elif user_input == "orange":
+    print("Orange you glad I didn't say banana.")
+elif user_input == "black":
+    print("I see a red door and I want it painted black")
+elif user_input == "purple":
+    print("And we'll never be royalllssss")
+elif user_input == "pink":
+    print("Pinky- and the Brain")
+else:
+    print("I don't recognize that color. Is it even...??")
+
+Prediction:     It will print one of those responses corrisponding with the color the user chose
+Actual:         Did exactly how i predicted
+
+---------------------------------------
+In your Notebook
+---------------------------------------
+Translate this Snap! program into a Python program
+***Refer to the image provided on Moodle located below the Lab 2.03 link***
+Write program below:
+x = input ("what is x")
+y = input ("what is y")
+z = input ("what is z")
+
+---------------------------------------
+Create a game show program
+---------------------------------------
+Declare 10 prizes (prize1, prize2, prize 3 at the top of your file)
+User picks a number
+The prize corresponding with that door is printed for the user.
+Write code below the multiline comment
+'''
+x = input ("what is x")
+y = input ("what is y")
+z = input ("what is z")
+if (x + y > z) and (x + z > y) and (y + z > x):
+    print('perueter of the triange is ' + (x + y + z))
+elif ()
--- user_input_challenge.py
+++ user_input_challenge.py
@@ -11,6 +11,7 @@ many copies of the previous message. (Hint: order of operations exists in Python
 Print out that many copies of the previous message on separate lines. 
 (Hint: the string "\n" is the same as pressing the ENTER button)
 '''
+
 #
 #
 #
@@ -20,5 +21,8 @@ Print out that many copies of the previous message on separate lines.
 
 print('I will be telling you when you will tell you when you turn 100.')
 user_name = input ('What is your name? ')
-user_age = input ('How old are you? ')
-when_will_user_turn_100 =
+user_age = int(input ('How old are you? '))
+birth_year = 2022 - user_age
+when_user_turns_100 = 100 + birth_year
+print(user_name + " you will turn 100 in the year ")
+print (when_user_turns_100)

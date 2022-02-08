562f0c138ac62d53542526968662d7a5d6279c83
Logan G <lpglendening25@students.bmchs.org>
2022-02-08 09:07:10 -0600

    done with tic tac toe game

--- Lab_2.06.py
+++ Lab_2.06.py
@@ -49,40 +49,79 @@ Print the updated board out.
 You will not need to determine the winner at this point.
 (Copy and paste your previous tic-tac-toe version and modify the code to implement the above)
 '''
-print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-print("---------")
-print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-print("---------")
-print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+# board
+
+board = [[1, 2, 3] , [4, 5, 6] , [7, 8, 9]]
+
+while True:
+    
+
+    print("---------")  
+    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+    print("---------")
+    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+    print("---------")
+    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+    print("---------")
 
 
 #player turn
 
-turn = int(input("pick a number to where to place your x: "))
+    turn = int(input("pick a number to where to place your x: "))
 
-if turn==1:
-    board[0][0] = 'x'
-elif turn==2:
-    board[0][1] = 'x'
-elif turn==3:
-    board[0][2] = 'x'
-elif turn==4:
-    board[1][0] = 'x'
-elif turn==5:
-    board[1][1] = 'x'
-elif turn==6:
-    board[1][2] = 'x'
-elif turn==7:
-    board[2][0] = 'x'
-elif turn==8:
-    board[2][1] = 'x'
-elif turn==9:
-    board[2][2] = 'x'
+    if turn==1:
+        board[0][0] = 'x'
+    elif turn==2:
+        board[0][1] = 'x'
+    elif turn==3:
+        board[0][2] = 'x'
+    elif turn==4:
+        board[1][0] = 'x'
+    elif turn==5:
+        board[1][1] = 'x'
+    elif turn==6:
+        board[1][2] = 'x'
+    elif turn==7:
+        board[2][0] = 'x'
+    elif turn==8:
+        board[2][1] = 'x'
+    elif turn==9:
+        board[2][2] = 'x'
+
 
 #update board
 
-print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
-print("---------")
-print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
-print("---------")
-print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+ 
+
+    print("---------")
+    print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+    print("---------")
+    print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+    print("---------")
+    print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
+    print("---------")
+
+
+#player turn
+
+    turn = int(input("pick a number to where to place your o: "))
+
+    if turn==1:
+        board[0][0] = 'o'
+    elif turn==2:
+        board[0][1] = 'o'
+    elif turn==3:
+        board[0][2] = 'o'
+    elif turn==4:
+        board[1][0] = 'o'
+    elif turn==5:
+        board[1][1] = 'o'
+    elif turn==6:
+        board[1][2] = 'o'
+    elif turn==7:
+        board[2][0] = 'o'
+    elif turn==8:
+        board[2][1] = 'o'
+    elif turn==9:
+        board[2][2] = 'o'
+
